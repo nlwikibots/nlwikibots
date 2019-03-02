a8f39ae3616aed598df9035ff5a843f97bdb6226
tools.nlwikibots <tools.nlwikibots@tools-bastion-03.tools.eqiad.wmflabs>
2019-03-02 20:39:30 +0000

    Convert tvpupdater to venv-based system

--- bots/tvpupdater/user-config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8  -*-
-family = 'wikipedia'
-mylang = 'nl'
-usernames['wikipedia']['nl'] = u'Nlwikibots' #u'Valhallasw-toolserver-botje'
-
-userinterface_init_kwargs = {'default_stream': 'stdout'}
-
-password_file = "../botpasswords" #".passwd"
